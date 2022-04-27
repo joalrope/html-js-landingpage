cc0d4d13074da33c7d29c50657714d8dae6d943b
Jose Rodriguez <joalrope@gmail.com>
2022-04-27 06:29:04 -0400

    section nabout refact

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,14 +1,14 @@
 $(function () {
-  $('main').on('scroll', function (e) {
-    const scroll = $('main').scrollTop();
+	$("main").on("scroll", function (e) {
+		const scroll = $("main").scrollTop();
 
-    if (scroll >= 165) {
-      $('.navbar').addClass('bg-primary text-white');
-      $('.nav-link').addClass('text-white');
-      $('.navbar').removeClass('container');
-    } else {
-      $('.navbar').removeClass('bg-primary text-white');
-      $('.nav-link').removeClass('text-white');
-    }
-  });
+		if (scroll >= 70) {
+			$(".navbar").addClass("bg-primary text-white");
+			$(".nav-link").addClass("text-white");
+			$(".navbar").removeClass("container");
+		} else {
+			$(".navbar").removeClass("bg-primary text-white");
+			$(".nav-link").removeClass("text-white");
+		}
+	});
 });
